3f2ae4214fe07a12c67b4dadab06ec2de9ee2f7e
Brandon Kirby <brandon.kirby@gsa.gov>
2015-09-17 12:39:20 -0400

    Start putting the logic for what is displayed of the form all in one place.

--- client/views/app.jsx
+++ client/views/app.jsx
@@ -2,29 +2,59 @@ App = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData: function() {
     return {
-    };
+      formState: Session.get("formState")
+    }
   },
   getInitialState: function() {
     return {
-      page: "section8"
+      activeSection: "section8"
     }
   },
   render: function() {
-    var page = this.state.page;
-    var subsection = this.state.subsection;
+    var activeSection = this.state.activeSection;
+    var formState = this.data.formState;
+
+    var renderSection8Instructions = function() {
+      return <PrevailingWageSurveyHourlyInstructions/>;
+    };
+
+    var renderSection8 = function() {
+      return <Section8/>;
+    };
     
-    var renderPage = function() {
-      switch (page) {
-        case "section8":
-          return <Section8/>;
-        default:
-          return <p>no valid page wired yet</p>;
-      }
+    var renderSection8 = function() {
+      return <Section8/>;
+    };
+    
+    var renderSection8a = function() {
+      return <Section8a/>;
+    };
+    
+    var renderSection8b = function() {
+      return <Section8b/>;
+    };
+    
+    var renderSection8bAlternate = function() {
+      return <Section8bAlternate/>;
+    };
+    
+    var renderSection8bPws = function() {
+      return <Section8bPws/>;
+    };
+    
+    var renderSection8bSca = function() {
+      return <Section8bSca/>;
     };
     
     return (
       <div>
-        { renderPage() }
+        { renderSection8Instructions() }
+        { renderSection8() }
+        { renderSection8a() }
+        { renderSection8b() }
+        { renderSection8bAlternate() }
+        { renderSection8bPws() }
+        { renderSection8bSca() }
       </div>
     );
   }
--- client/views/instructions/prevailingWageSurveyHourlyInstructions.jsx
+++ client/views/instructions/prevailingWageSurveyHourlyInstructions.jsx
@@ -1,10 +1,4 @@
 PrevailingWageSurveyHourlyInstructions = React.createClass({
-  mixins: [ReactMeteorData],
-  getMeteorData: function() {
-    return {
-      prevailingWageSurveyHourly: Session.get("prevailingWageSurveyHourly")
-    };
-  },
   render: function() {
     return (
       <p>Insert instructions for prevailing wage survey paid hourly wage rates instructions here</p>
